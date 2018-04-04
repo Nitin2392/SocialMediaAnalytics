fdd806c208d4dfd5166ce10d2a6352778b85e0eb
Nitin Rangarajan <nitinr92@gmail.com>
2018-04-04 01:35:17 -0400

    Python code to snoop on other Twitter users' stats

---
+++ TwitterDataOtherUser.py
@@ -0,0 +1,16 @@
+import json
+from tweepy import Cursor
+from TwitterMining import get_twitter_client
+
+if __name__ == '__main__':
+    user = "StellaFoxTrot"
+    client = get_twitter_client()
+
+    for status in Cursor(client.home_timeline).items(20):
+        print(status.text)
+
+    for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(4):
+        for status in page:
+            file = open("Twitter_data_userlevel.jsonl", "w")
+            file.write(json.dumps(status._json) + "\n")
+            file.close()
