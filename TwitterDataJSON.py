968229ac3e63cba3d88ef397aafa490990f11f1d
Nitin Rangarajan <nitinr92@gmail.com>
2018-04-24 13:30:14 -0400

    Updated Python Code to extract 800 self-tweets

--- TwitterDataJSON.py
+++ TwitterDataJSON.py
@@ -4,12 +4,12 @@ from TwitterMining import get_twitter_client
 
 if __name__ == '__main__':
     client = get_twitter_client()
-
+    i=0
     for status in Cursor(client.home_timeline).items(20):
-        print(status.text) #prints latest 20 tweets/ retweets        )
+        print(str(i) + " " + status.text)  # prints latest 20 tweets/retweets
+        i=i+1
 
-    for page in Cursor(client.home_timeline, count=200).pages(4):
-        for status in page:
-            file = open("Twitter_data.jsonl", "w")
-            file.write(json.dumps(status._json) + "\n")
-            file.close()
+    with open("Twitter_data.jsonl", "w") as write_to_file:
+        for page in Cursor(client.home_timeline, count=200).pages(4):
+            for status in page:
+                write_to_file.write(json.dumps(status._json) + "\n")
