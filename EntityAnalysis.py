8d2ea0da44c0bb3ca3f164905f51610f5653a5cd
Nitin Rangarajan <nitinr92@gmail.com>
2018-04-24 17:38:00 -0400

    Updated code to clear counter

--- EntityAnalysis.py
+++ EntityAnalysis.py
@@ -10,11 +10,12 @@ def get_hashtags(Tweet):
 if __name__ == '__main__':
     fname = "Twitter_data_userlevel.jsonl"
     with open(fname, "r") as fi:
-        hash = Counter() # Counter container helps us keep count of how many times equivalent values are added
+        hash_new = Counter() # Counter container helps us keep count of how many times equivalent values are added
+        hash_new.clear()
         for line in fi:
             tweet = json.loads(line)
             hashtags_in_tweet = get_hashtags(tweet)
-            hash.update(hashtags_in_tweet)
-        for tag,count in hash.most_common(20):
+            hash_new.update(hashtags_in_tweet)
+        for tag,count in hash_new.most_common(100):
             print("{}:{}".format(tag, count))
 
