c87946ba0841405869ebcb99b05fe770b9a29882
Nitin Rangarajan <nitinr92@gmail.com>
2018-04-24 17:36:40 -0400

    Updated User Mentions

--- Mentions_Analysis.py
+++ Mentions_Analysis.py
@@ -4,13 +4,14 @@ import json
 
 def get_mentions(Tweet):
     entities = Tweet.get('entities', {})
-    mentions = entities.get('mentions', {})
-    return [tag['text'].lower() for tag in mentions]
+    mentions = entities.get('user_mentions', {})
+    return [tag['name'].lower() for tag in mentions]
 
 if __name__ == '__main__':
     fname = "Twitter_data_userlevel.jsonl"
     with open(fname, "r") as fi:
         hash = Counter() # Counter container helps us keep count of how many times equivalent values are added
+        hash.clear()
         for line in fi:
             tweet = json.loads(line)
             mentions_in_tweet = get_mentions(tweet)
