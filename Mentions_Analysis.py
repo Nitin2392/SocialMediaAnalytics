f77b84b8120a2adbec40b00578afb8015a7fe5a6
Nitin Rangarajan <nitinr92@gmail.com>
2018-04-24 17:26:49 -0400

    Analyzing user mentions in Twitter

---
+++ Mentions_Analysis.py
@@ -0,0 +1,19 @@
+import sys
+from collections import Counter
+import json
+
+def get_mentions(Tweet):
+    entities = Tweet.get('entities', {})
+    mentions = entities.get('mentions', {})
+    return [tag['text'].lower() for tag in mentions]
+
+if __name__ == '__main__':
+    fname = "Twitter_data_userlevel.jsonl"
+    with open(fname, "r") as fi:
+        hash = Counter() # Counter container helps us keep count of how many times equivalent values are added
+        for line in fi:
+            tweet = json.loads(line)
+            mentions_in_tweet = get_mentions(tweet)
+            hash.update(mentions_in_tweet)
+        for tag, count in hash.most_common(20):
+            print("{}:{}".format(tag, count))
